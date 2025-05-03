8999d843304cb038b0b890fb544d8bb62e595b51
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-03 12:09:41 -0700

    changed it

--- init.lua
+++ init.lua
@@ -17,13 +17,9 @@ local lazy_config = require("config.lazy")
 
 require("lazy").setup({
 	spec = {
-		-- GodVim Plugins -- remove whichever modulules you like
-		{ "GodVim/GODVIM",                 branch = "main", import = "godvim.plugins" },
-		{ import = "godvim.plugins.cp", },
-		{ import = "godvim.plugins.ui" },
-		{ import = "godvim.plugins.lsp" },
-		{ import = "godvim.plugins.editor" },
-		{ import = "godvim.plugins.lang" },
+		-- GodVim Plugins -- remove whichever modules you like
+		{ "GodVim/GODVIM", dev=true, import = "godvim.plugins" },
+--    { import = "godvim.plugins.extras.cp" },
 		-- Override GodVim's plugins
 		{ import = "plugins" },
 	},
